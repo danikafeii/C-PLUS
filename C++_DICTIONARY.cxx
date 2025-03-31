60ab7b90d9d0e5ff3d178bc0995da0677bd6db6a
danikafeii <fheycruz153@gmail.com>
2025-04-01 00:10:26 +0800

    Add files via upload

---
+++ C++_DICTIONARY.cxx
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	string a;
+	
+	cout<<"Enter a wattpad names or slangs:";
+	cin>>a;
+	if(a=="DUKE"){
+		cout<<"AN ENGINEER AND BOYFRIEND OF SOLENE.";
+	}
+	else if(a=="SOLENE"){
+		cout<<"PASTRY CHEF AND GIRLFRIEND OF DUKE.";
+	}
+	else if(a=="TROY"){
+		cout<<"A PLAYBOY BUT LOYAL TO THE GIRL HE LOVES.";
+	}
+	else if(a=="CHIN"){
+		cout<<"THE WOMAN WHO TROY FELL IN LOVE.";
+	}
+	else if(a=="ROVINA"){
+		cout<<"SMART, AND A LOYAL FRIEND TO CHIN.";
+	}
+	else if(a=="CALIX"){
+		cout<<"LONG HAIR AND A SHY PERSON.";
+	}
+	else if(a=="REESE"){
+		cout<<"RICH AND OBSSESSED EX OF HARVIN.";
+	}
+	else if(a=="HARVIN"){
+		cout<<"A TOURISM STUDENT THAT CAN PLAY GUITAR.";
+	}
+	else if(a=="FAYE"){
+		cout<<"A GIRL WHO'S LIVING IN THE BOOK WORLD.";
+	}
+	else if(a=="SEBASTIAN"){
+		cout<<"THE AUTHOR OF THE BOOK WHERE FAYE WAS IN.";
+	}
+	else if(a=="HATSUMI"){
+		cout<<"BEGINNING OF BEAUTY.";
+	}
+	else if(a=="CHIYO"){
+		cout<<"A THOUSAND SPARKLES, FOREVER.";
+	}
+	else if(a=="KAYOKO"){
+		cout<<"CHILD OF A GOOD GENERATION.";
+	}
+	else if(a=="JACOB"){
+		cout<<"BAD BOY OF THE MONTH.";
+	}
+	else if(a=="WADE"){
+		cout<<"FAVORITE BILLIARD PLAYER OF THE BOYS.";
+	}
+	else if(a=="RENNY"){
+		cout<<"CURLY GIRL WHO IS GOOD AT DAMATH.";
+	}
+	else if(a=="TINA"){
+		cout<<"A NONCHALANT GIRL WHO LOVES TO TALK TO HER FAVORITE PERSONS.";
+	}
+	else if(a=="TANTAN"){
+		cout<<"A BOY WHO'S GOOD AT EVERYTHING, ESPECIALLY MAKING MY HEART SKIP A BEAT CAUSE OF HIS SMILE.";
+	}
+	else if(a=="ANNIE"){
+		cout<<"BESTFRIEND OF TANTAN, HIS PROBLEM SOLVING BUDDY.";
+	}
+	else if(a=="DREI"){
+		cout<<"GOT REJECTED BY ANNIE.";
+	}
+	else if(a=="SHARRY"){
+		cout<<"JOYFUL AND ENERGETIC FRIEND OF DREI, ANNIE, AND FAYE.";
+	}
+	else if(a=="JEFF"){
+		cout<<"THE ALWAYS 'LATE SA BALITA' TYPA FRIEND.";
+	}
+	else if(a=="KENNY"){
+		cout<<"MATH WIZARD THAT INLOVE TO A NURSING STUDENT.";
+	}
+	else if(a=="YVONA"){
+		cout<<"THE NURSING STUDENT, IFYKYK.";
+	}
+	else if(a=="HANNABELLA"){
+		cout<<"INTROVERT FRIEND OF YVONA LEWIS.";
+	}
+	else if(a=="THRAIA"){
+		cout<<"ALWAYS THE THIRDWHEEL FRIEND.";
+	}
+	else if(a=="NICS"){
+		cout<<"FOODY LOVER, THE ALWAYS 'GUTOM' IN GROUPM?";
+	}
+	else if(a=="RADLEIGH"){
+		cout<<"THE ONE WHO SAVE LUCY.";
+	}
+	else if(a=="LEANDRO"){
+		cout<<"THE PLAYBOY AND UNAFFECTED OF REJECTION.";
+	}
+	else if(a=="YNESSA"){
+		cout<<"THE NORMAL GIRL WHO GAVE UP THE 10 MILLION PESOS FOR A BLUEBERRY CHEESECAKE.";
+	}
+	else{
+		cout<<"WALEY TEH";
+	}
+	return 0;
+
+}
